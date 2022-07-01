2d187037e05971ac468020a927143965f653d474
Flight64 <ajosekuruvilla@gmail.com>
2022-07-01 18:03:19 +0530

    Decoded BMS data

--- TTGO_BMS_Test/TTGO_BMS_Test.ino
+++ TTGO_BMS_Test/TTGO_BMS_Test.ino
@@ -1,17 +1,15 @@
-#include <ModbusClientRTU.h>
 #include "BMSHandler.h"
-
-ModbusClientRTU RS485(Serial1);
-BMSHandler bms(&RS485);
 
 void setup()
 {
     Serial.begin(115200);
     Serial1.begin(19200, SERIAL_8N1, 13, 15);
-    bms.initBMS();
-    bms.updateBMSTelemetry();
+    initBMS();
 }
 
 void loop()
 {
+    updateBMSTelemetry();
+    getBMSTelemetry();
+    delay(1000);
 }
