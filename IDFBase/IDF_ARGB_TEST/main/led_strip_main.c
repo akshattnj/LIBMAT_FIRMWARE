9928b6730e7019baafd9e4c03a8271e4315d1f2c
akshattnj <akshattnj@gmail.com>
2023-02-22 11:57:25 +0530

    removed kconfig dependency

--- IDFBase/IDF_ARGB_TEST/main/led_strip_main.c
+++ IDFBase/IDF_ARGB_TEST/main/led_strip_main.c
@@ -11,15 +11,17 @@ static const char *TAG = "v1.0";
 #define RMT_TX_CHANNEL RMT_CHANNEL_0
 
 #define EXAMPLE_CHASE_SPEED_MS (100) //speed of the blinking
+#define GPIO_PIN_ARGB (15)
+#define Num_LED (4) //number of leds
 
 void app_main(void)
 {
-    uint32_t red = 255;
+    uint32_t red = 0;
     uint32_t green = 255;
-    uint32_t blue = 255;
-    uint16_t start_rgb = 0;
+    uint32_t blue = 0;
 
-    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(CONFIG_EXAMPLE_RMT_TX_GPIO, RMT_TX_CHANNEL);
+
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(GPIO_PIN_ARGB, RMT_TX_CHANNEL);
     // set counter clock to 40MHz
     config.clk_div = 2;
 
@@ -27,7 +29,7 @@ void app_main(void)
     ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
 
     // install ws2812 driver
-    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(CONFIG_EXAMPLE_STRIP_LED_NUMBER, (led_strip_dev_t)config.channel);
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(Num_LED, (led_strip_dev_t)config.channel);
     led_strip_t *strip = led_strip_new_rmt_ws2812(&strip_config);
     if (!strip) {
         ESP_LOGE(TAG, "install WS2812 driver failed");
@@ -38,16 +40,19 @@ void app_main(void)
     ESP_LOGI(TAG, "LED Rainbow Chase Start");
     while (true) {
         
-            /* for (int j = 0; j < CONFIG_EXAMPLE_STRIP_LED_NUMBER8; j += 1) {
+            /* for (int j = 0; j < Num_LED8; j += 1) {
                 // Write RGB values to strip driver
                 ESP_ERROR_CHECK(strip->set_pixel(strip, j, red, green, blue));
             } */
 
             //set rgb values
-            ESP_ERROR_CHECK(strip->set_pixel(strip, 0, 0, 0, 255)); 
-            ESP_ERROR_CHECK(strip->set_pixel(strip, 1, 0, 0, 255));
-            ESP_ERROR_CHECK(strip->set_pixel(strip, 2, 0, 0, 255)); 
-            ESP_ERROR_CHECK(strip->set_pixel(strip, 3, 0, 0, 255));
+            for (int i = 0; i < Num_LED; i++)
+            {
+                 ESP_ERROR_CHECK(strip->set_pixel(strip, i, red, green, blue));
+            }
+            
+            
+
             ESP_ERROR_CHECK(strip->refresh(strip, 100));
             vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
             //strip->clear(strip, 500);
