307bccd79f3a36093a35d9a42767fc7c08fa9e7b
Antony Kuruvilla <ajosekuruvilla@gmail.com>
2023-03-06 02:52:21 +0530

    Moved to .h file and created animations

---
+++ IDFBase/IDF_ARGB_TEST/main/main.cpp
@@ -0,0 +1,40 @@
+#include "src/LEDHandler.h"
+#include "src/definations.h"
+
+extern "C"
+{
+    #include <stdio.h>
+}
+
+LEDHandler ledHandler;
+
+extern "C" void app_main(void)
+{
+    ledHandler.init();
+    xTaskCreate([](void *params){ledHandler.ledAnimationTask(params);}, "ledAnimationTask", 2048, NULL, 10, NULL);
+
+    while (1)
+    {
+        ledHandler.setBatteryPercentage(100);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ledHandler.setBatteryPercentage(50);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ledHandler.setBatteryPercentage(25);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ledHandler.setBatteryPercentage(0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        ledHandler.setAnimationSelection(1);
+        ledHandler.setBatteryPercentage(100);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ledHandler.setBatteryPercentage(50);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ledHandler.setBatteryPercentage(25);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ledHandler.setBatteryPercentage(0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        ledHandler.setAnimationSelection(2);
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
+    }
+}
---
+++ IDFBase/IDF_ARGB_TEST/main/src/definations.h
@@ -0,0 +1,7 @@
+#ifndef DEFINATIONS_H
+#define DEFINATIONS_H
+
+#define NEOPIXEL_PIN 16
+#define NEOPIXEL_NUM 5
+
+#endif
