a9db3e541b0673993c7097db34841abc270d5fb2
Antony Kuruvilla <ajosekuruvilla@gmail.com>
2023-03-14 01:36:59 +0530

    TODO for wired interfaces

--- IDFBase/Main/IDF_Main_Hub/main/src/Wired/CANHandler.cpp
+++ IDFBase/Main/IDF_Main_Hub/main/src/Wired/CANHandler.cpp
@@ -1,6 +1,17 @@
-#include "CANHandler.h"
+/**
+ * CAN RX GPIO: ??
+ * CAN TX GPIO: ??
+ * 
+ * TODO:
+ * 1) CAN general scan and pass results to GPIO handler
+ * 2) CAN ping and response for incoming battery
+ * 3) CAN ping and response loss for outgoing battery
+ * 4) CAN transmit to battery
+ * 5) CAN telemetry scan and pass to telemetry handler
+*/
 
+#include "CANHandler.h"
 namespace CAN
 {
-    
+
 }
--- IDFBase/Main/IDF_Main_Hub/main/src/Wired/GPIOHandler.cpp
+++ IDFBase/Main/IDF_Main_Hub/main/src/Wired/GPIOHandler.cpp
@@ -1,6 +1,46 @@
+/**
+ * General shape of the hub
+ * 
+ * |------------------------------------|
+ * |                                    |
+ * |    ------------    ------------    |
+ * |    | ?? ?? ?? |    | ?? ?? ?? |    | Charger Sensor Lock GPIO
+ * |    |     3    |    |     1    |    | Slot Number
+ * |    |   21-27  |    |   7-13   |    | Neopixel Order
+ * |    |   0x00E  |    |   0x00C  |    | Slot CAN ID
+ * |    ------------    ------------    |
+ * |                                    |
+ * |    ------------    ------------    |
+ * |    | ?? ?? ?? |    | ?? ?? ?? |    | Charger Sensor Lock GPIO
+ * |    |     2    |    |     0    |    | Slot Number
+ * |    |   14-20  |    |    0-6   |    | Neopixel Order
+ * |    |   0x00D  |    |   0x00B  |    | Slot CAN ID
+ * |    ------------    ------------    |
+ * |                                    |
+ * |------------------------------------|
+ * 
+ * Master CAN ID 0x00A
+ * General response ping ID 0xFFF
+ * 
+ * TODO:
+ * 1) Identify initial state
+ *     a) General response ping
+ *     b) Accept responses for 3 seconds
+ *     c) Map battries to slots
+ * 2) Initialise GPIO based on initial state
+ * 3) Handle swap
+ *     a) Identify slot with swappable battery --->>> Error Path - No swappable battery
+ *     b) Assign CAN ID to incoming battery
+ *     c) Scan for incoming battery
+ *     d) Scan for incoming battery door closed
+ *     e) Change charger states
+ *     f) Scan for outgoing battery removal
+ *     g) Scan for outgoing battery gate closed
+*/
+
 #include "GPIOHandler.h"
 
 namespace GPIO
 {
-    
+
 }
